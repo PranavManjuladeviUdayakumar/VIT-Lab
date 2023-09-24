f463e73652df454e19708e6fc1588c74ad5a860b
PranavManjuladeviUdayakumar <PranavManjuladeviUdayakumar@gmail.com>
2023-09-25 00:05:46 +0530

    Corrected value processing

--- LRL.py
+++ LRL.py
@@ -3,12 +3,12 @@ from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 
 
-l = []
+l0, l1 = [], []
 print("Enter two numbers separated by a space in the form of 'x_value y_value' to store values or enter a blank input to start processing stored input")
 while True:
     x = input("Input: ").lstrip().rstrip()
     if (x == '' or x == '/n'):
-        if len(l) < 2:
+        if len(l0) < 2:
             print("Cannot proceed without storing atleast 2 pairs of data")
             pass
         else:
@@ -19,22 +19,22 @@ while True:
         if len(vals) != 2:
             print("Incorrect number of numbers entered")
         else:
-            l.append([int(vals[0]), int(vals[1])])
+            l0.append(float(vals[0]))
+            l1.append(float(vals[1]))
 
-
-x = np.array(l[0]).reshape((-1, 1))
-y = np.array(l[1])
+x = np.array(l0).reshape((-1, 1))
+y = np.array(l1)
 
 model = LinearRegression().fit(x, y)
-slope, intercept = int(model.coef_[0]), int(model.intercept_)
+slope, intercept = float(model.coef_[0]), float(model.intercept_)
 print(f"Slope: {slope}")
 print(f"Equation: y = {slope}x + {intercept}")
 
-plt.scatter(l[0], l[1])
+plt.scatter(l0, l1)
 
 points = []
-for i in l[0]:
+for i in l0:
     points.append(slope*i + intercept)
 
-plt.plot(l[0], points)
+plt.plot(l0, points)
 plt.show()
